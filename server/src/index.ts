2974c0df3c1124a423ad8ed1cda70805ba717aa3
hira sadaf <hirahsadaf@gmail.com>
2023-12-26 01:17:52 -0600

    added mock data for tracks

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs, mocks } from "./schema";
+import { addMocksToSchema } from "@graphql-tools/mock";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+async function startApolloServer() {
+    const server = new ApolloServer({
+        schema: addMocksToSchema({
+            schema: makeExecutableSchema({ typeDefs }),
+            mocks
+        }),
+    });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+      🚀  Server is running!
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
